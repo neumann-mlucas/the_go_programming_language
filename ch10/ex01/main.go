4230c333a3bb27ae082bd7b6b15f4fb65ba981d7
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-01-28 12:28:22 -0300

    add ch10 ex01

---
+++ ch10/ex01/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"image"
+	"image/gif"
+	_ "image/gif"
+	"image/jpeg"
+	_ "image/jpeg"
+	"image/png"
+	_ "image/png"
+	"io"
+	"os"
+)
+
+var formatPtr = flag.String("f", "jpg", "image format")
+
+func main() {
+	flag.Parse()
+	if err := convert(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func convert(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+
+	switch *formatPtr {
+	case "jpg":
+		return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
+	case "png":
+		return png.Encode(out, img)
+	case "gif":
+		return gif.Encode(out, img, &gif.Options{})
+	default:
+		return errors.New("Output format " + *formatPtr + " in not supported")
+	}
+}
